0fb6d572bb78ffc225ec145b0a29ff2164ec2e86
Battseren Badral <bbattseren88@gmail.com>
2026-01-26 17:08:43 +0100

    Removing all

--- orontts/inference/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Inference module for VITS2 synthesis."""
-
-from orontts.inference.synthesizer import Synthesizer
-from orontts.inference.audio_output import AudioOutput
-
-__all__ = [
-    "Synthesizer",
-    "AudioOutput",
-]
--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tests for OronTTS package."""
